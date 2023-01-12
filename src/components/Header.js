d26438a2724addb63bb4aecb47e02293b61f7643
renzgabrinao <50391561+renzgabrinao@users.noreply.github.com>
2023-01-11 19:45:40 -0800

    animate nav bar on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,32 @@
 // header page
 
-import Nav from './Nav'
-import logo from '../globals/images/app-logo.png'
-import { Link } from 'react-router-dom'
+import Nav from "./Nav";
+import logo from "../globals/images/app-logo.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <header className='header'>
-      <Link to='/MovieDB'><img src={logo} alt='Logo for app' className='logo'/></Link>
-      <Nav/>
+    <header className={`header ${toggle ? "active" : ""}`}>
+      <div className="header-container">
+        <Link to="/MovieDB">
+          <img src={logo} alt="Logo for app" className="logo" />
+        </Link>
+        <div
+          className="toggle-button"
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </div>
+      <Nav toggle={toggle} />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,23 @@
 // nav page
 import { Link } from "react-router-dom";
-import { useState } from 'react'
 
-const Nav = () => {
-
-  // const navbarLinks = document.getElementsByClassName('nav-item')
-  const [ toggle, setToggle ] = useState(false)
-
-
-  console.log(toggle)
-
+const Nav = ({ toggle }) => {
   return (
     <>
-      <div className="toggle-button" onClick={() => {setToggle(!toggle)}}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-      <nav className='nav-bar'>
-        <Link to='/MovieDB' className={`nav-item ${toggle ? "active" : ""}`}><p>Home</p></Link>
-        <Link to='/MovieDB/favourites' className={`nav-item ${toggle ? "active" : ""}`}><p>Favourites</p></Link>
-        <Link to='/MovieDB/about' className={`nav-item ${toggle ? "active" : ""}`}><p>About</p></Link>
+      <nav className={`nav-bar ${toggle ? "active" : ""}`}>
+        <Link to="/MovieDB" className="nav-item">
+          <p>Home</p>
+        </Link>
+        <Link to="/MovieDB/favourites" className="nav-item">
+          <p>Favourites</p>
+        </Link>
+        <Link to="/MovieDB/about" className="nav-item">
+          <p>About</p>
+        </Link>
         {/* <Link to=''></Link> */}
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
