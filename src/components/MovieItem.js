a2188020ec9442c9be9e048ac3d73888f72b00b1
Renz Gabrinao <rgabrinao1@gmail.com>
2023-01-13 17:35:44 -0800

    stylingssss

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,79 +1,89 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import StarRatings from 'react-star-ratings';
 import {
-  addToLocalStorage,
-  removeFromLocalStorage,
-  ifMovieInFavourites,
-  getFavouriteMovies,
-} from "../data/utilities";
-import { useContext } from "react";
-import FavouriteContext from "../contexts/FavouriteContext";
-import "../styles/MovieItem.css";
+	addToLocalStorage,
+	removeFromLocalStorage,
+	ifMovieInFavourites,
+	getFavouriteMovies,
+} from '../data/utilities';
+import { useContext } from 'react';
+import FavouriteContext from '../contexts/FavouriteContext';
+import '../styles/MovieItem.css';
 
 const MovieItem = ({ movie }) => {
-  const { setFavourites } = useContext(FavouriteContext);
+	const { setFavourites } = useContext(FavouriteContext);
 
-  let movieOverview = movie.overview;
-  if (movieOverview.length > 100) {
-    movieOverview = movieOverview.slice(0, 74) + " ...";
-  }
+	let movieOverview = movie.overview;
+	if (movieOverview.length > 100) {
+		movieOverview = movieOverview.slice(0, 74) + ' ...';
+	}
 
-  const handleClick = (movie, method) => {
-    switch (method) {
-      case "add":
-        addToLocalStorage(movie);
-        break;
-      case "remove":
-        removeFromLocalStorage(movie);
-        break;
-      default:
-        break;
-    }
-    let favs = getFavouriteMovies();
-    setFavourites(favs);
-  };
+	const handleClick = (movie, method) => {
+		switch (method) {
+			case 'add':
+				addToLocalStorage(movie);
+				break;
+			case 'remove':
+				removeFromLocalStorage(movie);
+				break;
+			default:
+				break;
+		}
+		let favs = getFavouriteMovies();
+		setFavourites(favs);
+	};
 
-  return (
-    <div className="movie-item">
-      <div className="movie-item-details">
-        <img
-          src={"https://image.tmdb.org/t/p/w780" + movie.poster_path}
-          alt={"Poster for " + movie.original_title}
-        />
+	return (
+		<div className="movie-item">
+			<div className="movie-item-details">
+				<img
+					src={'https://image.tmdb.org/t/p/w780' + movie.poster_path}
+					alt={'Poster for ' + movie.original_title}
+				/>
 
-        <span>
-          <h2>{movie.original_title}</h2>
-          <p>Release Date: {movie.release_date}</p>
-          <p>User Score: {`${movie.vote_average * 10}%`}</p>
-          <div className="buttons">
-            <Link to={"/movie/" + movie.id}>
-              <button>More Info</button>
-            </Link>
-            {ifMovieInFavourites(movie) ? (
-              <button
-                onClick={() => {
-                  handleClick(movie, "remove");
-                }}
-              >
-                Remove from Favourites
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  handleClick(movie, "add");
-                }}
-              >
-                Add to Favourites
-              </button>
-            )}
-          </div>
-        </span>
-      </div>
+				<span>
+					<h2>{movie.original_title}</h2>
+					<p>Release Date: {movie.release_date}</p>
 
-      <div className="movie-description">
-        <article>{movieOverview}</article>
-      </div>
-    </div>
-  );
+					<StarRatings
+						rating={movie.vote_average / 2}
+						starRatedColor="yellow"
+						numberOfStars={5}
+						name="rating"
+						starDimension="30px"
+						starSpacing="1px"
+					/>
+
+					<div className="buttons">
+						<Link to={'/movie/' + movie.id}>
+							<button>More Info</button>
+						</Link>
+						{ifMovieInFavourites(movie) ? (
+							<button
+								onClick={() => {
+									handleClick(movie, 'remove');
+								}}
+							>
+								Remove from Favourites
+							</button>
+						) : (
+							<button
+								onClick={() => {
+									handleClick(movie, 'add');
+								}}
+							>
+								Add to Favourites
+							</button>
+						)}
+					</div>
+				</span>
+			</div>
+
+			<div className="movie-description">
+				<article>{movieOverview}</article>
+			</div>
+		</div>
+	);
 };
 
 export default MovieItem;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,100 +1,124 @@
 // movie detail page
-import { useState, useEffect, useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useState, useEffect, useContext } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import StarRatings from 'react-star-ratings';
 import {
-  ifMovieInFavourites,
-  addToLocalStorage,
-  removeFromLocalStorage,
-  getFavouriteMovies,
-} from "../data/utilities";
-import button from "../globals/images/back-button.png";
-import FavouriteContext from "../contexts/FavouriteContext";
+	ifMovieInFavourites,
+	addToLocalStorage,
+	removeFromLocalStorage,
+	getFavouriteMovies,
+} from '../data/utilities';
+import button from '../globals/images/back-button.png';
+import FavouriteContext from '../contexts/FavouriteContext';
 
 const MOVIE_DB_API_KEY = process.env.REACT_APP_MOVIE_DB_API_KEY;
 
 const MovieDetail = () => {
-  const { setFavourites } = useContext(FavouriteContext);
-  const { movieId } = useParams();
-  const [movie, setMovie] = useState("");
+	const { setFavourites } = useContext(FavouriteContext);
+	const { movieId } = useParams();
 
-  useEffect(() => {
-    const getMovie = async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIE_DB_API_KEY}&language=en-US`
-      );
-      const data = await response.json();
-      setMovie(data);
-    };
-    getMovie();
-  }, [movieId]);
+	const [movie, setMovie] = useState({});
+	const [genres, setGenres] = useState([]);
 
-  const handleClick = (movie, method) => {
-    switch (method) {
-      case "add":
-        addToLocalStorage(movie);
-        break;
-      case "remove":
-        removeFromLocalStorage(movie);
-        break;
-      default:
-        break;
-    }
-    let favs = getFavouriteMovies();
-    setFavourites(favs);
-  };
+	useEffect(() => {
+		const getMovie = async () => {
+			const response = await fetch(
+				`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIE_DB_API_KEY}&language=en-US`
+			);
+			const data = await response.json();
+			setMovie(data);
+			setGenres(data.genres);
+		};
+		getMovie();
+	}, [movieId]);
 
-  return (
-    <>
-      <div
-        className="movie-details"
-        style={{
-          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.poster_path}")`,
-          backgroundPosition: "center",
-          objectFit: "cover",
-          backgroundColor: "rgba(0,0,0,0.85)",
-          backgroundBlendMode: "multiply",
-        }}
-      >
-        <Link to="/" className="back-button">
-          <img src={button} alt="Back Button" />
-        </Link>
-        <img
-          className="poster-img"
-          src={"https://image.tmdb.org/t/p/original" + movie.poster_path}
-          alt={"Poster for " + movie.original_title}
-        />
-        <span className="movie-info">
-          <h2 className="movie-detail-title info">{movie.original_title}</h2>
-          <p className="release-date info">
-            Release Date: {movie.release_date}
-          </p>
-          <p className="rating info">
-            User Score: {`${Math.round(movie.vote_average * 10)}%`}
-          </p>
-          <article className="movie-overview info">{movie.overview}</article>
-          <div className="buttons">
-            {ifMovieInFavourites(movie) ? (
-              <button
-                onClick={() => {
-                  handleClick(movie, "remove");
-                }}
-              >
-                Remove from Favourites
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  handleClick(movie, "add");
-                }}
-              >
-                Add to Favourites
-              </button>
-            )}
-          </div>
-        </span>
-      </div>
-    </>
-  );
+	console.log(movie);
+
+	const handleClick = (movie, method) => {
+		switch (method) {
+			case 'add':
+				addToLocalStorage(movie);
+				break;
+			case 'remove':
+				removeFromLocalStorage(movie);
+				break;
+			default:
+				break;
+		}
+		let favs = getFavouriteMovies();
+		setFavourites(favs);
+	};
+
+	return (
+		<>
+			<div
+				className="movie-details"
+				style={{
+					backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.poster_path}")`,
+					backgroundPosition: 'center',
+					objectFit: 'cover',
+					backgroundColor: 'rgba(0,0,0,0.85)',
+					backgroundBlendMode: 'multiply',
+					// minHeight: '100vh',
+				}}
+			>
+				<Link to="/" className="back-button">
+					<img src={button} alt="Back Button" />
+				</Link>
+				<img
+					className="poster-img"
+					src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
+					alt={'Poster for ' + movie.original_title}
+				/>
+				<div className="movie-details-info">
+					<h2>{movie.original_title}</h2>
+					<h3>Release Date: {movie.release_date}</h3>
+					{movie.vote_average ? (
+						<>
+							<StarRatings
+								rating={movie.vote_average / 2}
+								starRatedColor="yellow"
+								numberOfStars={5}
+								name="rating"
+								starDimension="30px"
+								starSpacing="1px"
+							/>
+						</>
+					) : (
+						<></>
+					)}
+					<article>{movie.overview}</article>
+					<div className="genre-list">
+						{genres.map((genre) => (
+							<>
+								<h3 key={genre.id}>{genre.name}</h3>
+							</>
+						))}
+					</div>
+
+					<div className="buttons">
+						{ifMovieInFavourites(movie) ? (
+							<button
+								onClick={() => {
+									handleClick(movie, 'remove');
+								}}
+							>
+								Remove from Favourites
+							</button>
+						) : (
+							<button
+								onClick={() => {
+									handleClick(movie, 'add');
+								}}
+							>
+								Add to Favourites
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default MovieDetail;
